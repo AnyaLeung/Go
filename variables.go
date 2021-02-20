8a2e31a1ca6c2f0bf6a2c2af514aa25375edb817
anyaleung <anyaliangww@gmail.com>
2021-02-21 02:42:53 +0800

    Add

---
+++ variables.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+    
+    var a string = "initial"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+    
+    var d = true
+    fmt.Println(d)
+
+    var e int //init 0
+    fmt.Println(e)
+
+    f := "short" //declare and init
+    fmt.Println(f)
+}
